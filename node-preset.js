434e77d1b42e5a8345d988cb0740a4531b1e25ca
Soares Chen <soares.chen@gmail.com>
2016-06-27 00:13:50 +0800

    Add JSX plugin with pragma set to 'h'.

--- node-preset.js
+++ node-preset.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
-    require("babel-plugin-transform-async-to-generator"),
-    require("babel-plugin-transform-es2015-modules-commonjs"),
-    require("babel-plugin-transform-function-bind"),
+    require('babel-plugin-transform-async-to-generator'),
+    require('babel-plugin-transform-es2015-modules-commonjs'),
+    require('babel-plugin-transform-function-bind'),
+    [require('babel-plugin-transform-react-jsx'), { pragma: 'h' }]
   ]
 }
